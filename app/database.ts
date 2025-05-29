ae4a007e58ab8fc12739528a7706611198f99658
Shaswat Shrivas <103164388+ShaswatShrivas@users.noreply.github.com>
2025-05-29 21:56:33 +0530

    Implemented Route for API calls and Created Route Handlers for User

---
+++ app/database.ts
@@ -0,0 +1,28 @@
+const tickets = [
+  {
+    id: 1,
+    name: "How to learn React?",
+    status: "open",
+    type: "question",
+  },
+    {
+        id: 2,
+        name: "How to fix async functions?",
+        status: "closed",
+        type: "bug",
+    },
+    {
+        id: 3,
+        name: "How to learn Node.js?",
+        status: "open",
+        type: "question",
+    },
+    {
+        id: 4,
+        name: "How to learn TypeScript?",
+        status: "closed",
+        type: "question",
+    },
+];
+
+export default tickets;
